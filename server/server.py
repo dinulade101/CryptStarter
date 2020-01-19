709abc409d51ccceef154dafac1644d7cdfa2d05
Kai Bailey <kbailey1@ualberta.ca>
2020-01-19 09:57:42 -0700

    Enabled cors in flask

--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import sqlite3
 from dbhelper import DBHelper
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 db = DBHelper()
 
 
@@ -13,6 +16,7 @@ def hello_world():
 
 
 @app.route('/api/createpost', methods=['POST', 'GET'])
+@cross_origin()
 def createpost():
     if request.method == 'POST':
         pic = request.args.get('pic')
@@ -24,7 +28,6 @@ def createpost():
         pic_link = request.args.get('pic_link')
         db.create_post(id, pic_link)
 
-
         return jsonify("success")
 
 
@@ -33,7 +36,7 @@ def incrementlikes():
     if request.method == 'POST':
         id = request.args.get('id')
         db.increment_likes(id)
-        
+
         return jsonify("success")
 
 
@@ -43,7 +46,7 @@ def getpost():
         id = request.args.get('id')
         post_details = db.get_post_likes_comments(id)
         print(post_details)
-        
+
         return jsonify([{'likes': post_details[0]}, {'pic_link': post_details[1]}])
 
 
