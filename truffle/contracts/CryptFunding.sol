049f60392553504cf6ffeaa5ef20d2be8bf4179f
dinulade101 <dinulasd@gmail.com>
2020-01-18 22:07:50 -0700

    almost working smart contract

--- truffle/contracts/CryptFunding.sol
+++ truffle/contracts/CryptFunding.sol
@@ -2,63 +2,123 @@ pragma solidity 0.5.12;
 
 // help from here
 // https://programtheblockchain.com/posts/2018/01/19/writing-a-crowdfunding-contract-a-la-kickstarter/
+// https://medium.com/openberry/creating-a-simple-crowdfunding-dapp-with-ethereum-solidity-and-vue-js-69ddb8e132dd
 
 contract CryptFunding {
-    uint256 campaignCounter;
+    mapping (address => Campaign) campaigns;
 
-    struct Campaign {
-        uint256 id;
-        string title;
-        string longDescription;
-        address owner;
-        uint256 deadline;
-        uint256 goal;
-        uint256 raised;
-        mapping (address => uint256) contributions;
-    }
-
-    mapping(uint256 => Campaign) public campaigns;
-
-<<<<<<< HEAD
     // map {address : amount of money pledged}
     mapping(address => uint256) public pledges;
 
-    function CreateCampaign(string _title, string _long_description, address _owner, uint256 _deadline, uint256 _goal, uint256 _raised) public {
-        campaignCounter++;
+    // external functions use less gas because external functions can be read directly from call data
+    function createCampaign(uint256 numHours, uint256 goalAmount, string calldata newTitle, string calldata newDescription) external {    
+        Campaign newCampaign = new Campaign(
+            msg.sender,
+            newTitle,
+            newDescription,
+            now + (numHours * 1 hours),
+            goalAmount
+        );
 
-        campaigns[campaginCounter] = Campaign(campaginCounter, _title, _longDescription, _owner, _deadline, _goal, _raised);
+        address campaignAddress = address(newCampaign);
+
+        campaigns[campaignAddress] = newCampaign;
     }
 
-    function ReleaseFunds(uint256 id) private {
+    // function getCampagins() external view returns (Campaign[] memory) {
+    //     return campaigns;
+    // }
+
+    // function DeleteCampaign(uint256 id) public {
+    //     require(campaigns[id]);
+        
+    //     delete campaigns[id];
+    // }
+}
+
+contract Campaign {
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
+    }
+
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    address payable public owner;
+    string title;
+    string description;
+    uint256 deadline;
+    uint256 goal;
+    uint256 raised;
+    State state;
+
+    mapping (address => uint256) contributions;
+
+    constructor(
+        address payable _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _deadline,
+        uint256 _goal
+    ) public {
+        owner = _owner;
+        title = _title;
+        description = _description;
+        deadline = _deadline;
+        goal = _goal;
+        raised = 0;
+        state = State.Fundraising;
+    }
+
+    function contribute() external inState(State.Fundraising) payable {
+        require(msg.sender != owner);
+        contributions[msg.sender] = contributions[msg.sender] + msg.value;
+        raised = raised + msg.value;
+        checkIfCampaignComplete();
+    }
+
+    function checkIfCampaignComplete() public {
+        if (raised >= goal) { 
+            state = State.Successful;
+            releaseFunds();
+        } else if (now > deadline) {
+            state = State.Expired;
+        }
+    }
+
+    function releaseFunds() internal inState(State.Successful) returns (bool) {
+        if (owner.send(raised)) {
+            state = State.Successful;
+            // emit creator paid
+            return true;
+        } 
+        // if code reaches here, money not sent to creator
+        return false;
+    }
+
+    function returnFunds() inState(State.Expired) returns (bool) {
         
     }
 
-    function ReturnFunds(uint256 id) private {
-
-=======
-    function CreateFund(uint256 numHours, uint256 goalAmount, string newTitle, string newDescription) public {
-        campaigns[msg.sender] = Campaign({
-            title: newTitle,
-            long_description: newDescription,
-            owner: msg.sender,
-            deadline: now + (numHours * 1 hours),
-            goal: goalAmount,
-            raised: 0
-        });
->>>>>>> b008e4fa954b2ae637e10a6b037f583b169ee5bf
-    }
-
-    function Pledge(uint256 id, uint256 amount) public payable {
-        require(campaigns[id]);
-
-        campaigns[id].raised += amount;
-
-        
-    }
-
-    function DeleteCampaign(uint256 id) public {
-        require(campaigns[id]);
-        
-        delete campaigns[id];
+    function getDetails() public view returns(
+        address payable _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _deadline,
+        State _state,
+        uint256 _raised,
+        uint256 _goal
+    ) {
+        _owner = owner;
+        _title = title;
+        _description = description;
+        _deadline = deadline;
+        _state = state;
+        _raised = raised;
+        _goal = goal;
     }
 }
